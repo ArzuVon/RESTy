0339e74133a1f4cdf9d1397bab180b3b4a7138b3
Von <107226923+ArzuVon@users.noreply.github.com>
2022-10-20 00:52:54 -0400

    Phase 2 and 3 completed technically and not designed

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from 'react'
+import './results.scss'
 
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+function Results(props) {
+
+  return (
+    <div className='container'>
+      <section className='form-section'>
+        <pre>
+          <h4>
+            {props.data ? JSON.stringify(props.data, undefined, 2) : "Please enter an API in the box, select a request method then hit Go!"}
+          </h4>
+        </pre>
       </section>
-    );
-  }
+    </div>
+  )
 }
 
 export default Results;
