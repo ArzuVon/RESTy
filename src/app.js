afd4de2658dbe47811beeb23c77d95d4b786a24e
Von <107226923+ArzuVon@users.noreply.github.com>
2022-10-20 00:39:13 -0400

    Switching out class based component to functional base and started some of phase 3

--- src/app.js
+++ src/app.js
@@ -1,48 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './app.scss';
 
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Form from './components/Form';
+import Results from './components/Results';
+import axios from 'axios';
 
-class App extends React.Component {
+function App(){
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
+const [data, setData] = useState(null); // gets returned in the from
+const [requestParams, setRequestParams] = useState({});
+
+// this function receives the form data from the form component and sets our requestParams object
+const callApi = (incomingObjectOnForm) => {
+  setRequestParams(incomingObjectOnForm);
   }
 
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    this.setState({data, requestParams});
-  }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
+// use effect hook runs every time its dependency list changes
+useEffect(() => {
+  async function fetchData() {
+    console.log('REQUEST PARAMETERS', requestParams);
+
+    if(requestParams.method === 'GET') {
+      await axios.get(requestParams.url).then((res) => {
+        setData(res);
+        console.log('RES: ', res);
+      }).catch((err) => console.log('ERROR: ', err)); 
+    }
+
+    if(requestParams.method === 'POST') {
+      await axios.post(requestParams.url, requestParams.body).then((res) => {
+        setData(res);
+        console.log('POST RES', res);
+        console.log('METHOD: ', requestParams.method);
+      }).catch((err) => console.log('ERROR: ', err));
+    }
+    
+    if(requestParams.method === 'PUT') {
+      await axios.put(requestParams.url).then((res) => {
+        console.log('PUT method was passed');
+        console.log('METHOD: ', requestParams.method);
+      }).catch((err) => console.log('ERROR: ', err));
+    }
+    
+    if(requestParams.method === 'DELETE') {
+      await axios.delete(requestParams.url).then((res) => {
+        console.log('DELETE method was passed');
+        console.log('METHOD', requestParams.method);
+      }).catch((err) => console.log('ERROR: ', err));
+    }
+
   }
+  // fetchData gets called when useEffect gets called
+  fetchData();
+  }, [requestParams]);  
+
+  return (
+    <>
+      <Header />
+      <span className='home-span'>
+        <div className='url'>
+          <h4>Method: {requestParams.method}</h4>
+          <h4>URL: {requestParams.url}</h4>
+        </div>
+      </span>
+      <Form handleRequestParams={callApi} />
+      <Results data={data} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
